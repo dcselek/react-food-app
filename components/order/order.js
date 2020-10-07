539c4f6ab081ee061e1dc15496655f1e2a424b9d
Doğan Can Selek <57669626+cselekk97@users.noreply.github.com>
2020-10-07 12:37:42 +0300

    add tiny order box

--- components/order/order.js
+++ components/order/order.js
@@ -3,9 +3,16 @@ import cn from "classnames";
 
 import styles from "./order.module.css";
 
-function Order({ className, src, alt, name = "", price = "",med }) {
+function Order({ className, src, alt, name = "", price = "", med, tiny }) {
   return (
-    <div className={cn(styles.container, className,med && styles.medContainer)}>
+    <div
+      className={cn(
+        styles.container,
+        className,
+        med && styles.medContainer,
+        tiny && styles.tinyContainer
+      )}
+    >
       <img src={src} alt={alt}></img>
       <div className={cn(styles.orderTexts)}>
         <div className={cn(styles.orderName)}>{name}</div>
--- stories/2-Order.stories.js
+++ stories/2-Order.stories.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import Order from '../components/order/order'
-import Kebab from '../public/kebab.jpg'
+import Order from "../components/order/order";
+import Kebab from "../public/kebab.jpg";
 
-export default{
-    title: 'Menu Box'
-}
+export default {
+  title: "Menu Box",
+};
 
-export const order = ()=>(
-    <div>
-    <Order name="Kebab" price='17$' src={Kebab}></Order>
-    <Order med name="Kebab" price='17$' src={Kebab}></Order>
-    </div>
-)
+export const orderNormal = () => (
+  <Order name="Kebab" price="17$" src={Kebab}></Order>
+);
+
+export const orderMedium = () => (
+  <Order med name="Kebab" price="17$" src={Kebab}></Order>
+);
+export const orderTiny = () => (
+  <Order tiny name="Veggie tomato mix" price="17$" src={Kebab}></Order>
+);
