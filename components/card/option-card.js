0213aac9cfd2f0b95975ec41656f46916724964a
Doğan Can Selek <57669626+cselekk97@users.noreply.github.com>
2020-10-22 02:27:19 +0300

    add Option-Card

--- components/card/option-card.js
+++ components/card/option-card.js
@@ -6,16 +6,45 @@ import Card from "./card";
 
 function OptionCard() {
   return (
+    <div>
+    <div className={styles.cardTitle}>
+      <span>Delivery Method</span>
+    </div>
     <Card height={156} width={315}>
       <div className={cn(styles.optionCard)}>
         <form>
-          <input name="Option" id="Door" type="radio" value="Door" />
-          <label htmlFor="Door">Door delivery</label>
-          <input name="Option" id="Pick" type="radio" value="Pick" />
-          <label htmlFor="Pick">Pick up</label>
+          <div>
+            <label className={cn(styles.label)}>
+              <input
+                name="Option"
+                id="Door"
+                type="radio"
+                value="Door"
+                checked="checked"
+              />
+              <span className={cn(styles.checkmark)}></span>
+              Door Delivery
+            </label>
+          </div>
+          <hr></hr>
+          <div>
+            
+            <label className={cn(styles.label)}>
+            <input
+              name="Option"
+              id="Pick"
+              type="radio"
+              value="Pick"
+              checked="checked"
+            />
+            <span className={cn(styles.checkmark)}></span>
+            Pick Up
+            </label>
+          </div>
         </form>
       </div>
     </Card>
+    </div>
   );
 }
 
