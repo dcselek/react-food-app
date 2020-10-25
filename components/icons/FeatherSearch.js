bb8ebb1f785ebe7818ef77d601c650a401968ade
Doğan Can Selek <57669626+cselekk97@users.noreply.github.com>
2020-10-25 22:55:16 +0300

    fix css

---
+++ components/icons/FeatherSearch.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+function SvgFeatherSearch(props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 122 122" fill="none" {...props}>
+      <path
+        d="M55.917 96.583c22.46 0 40.666-18.207 40.666-40.666 0-22.46-18.207-40.667-40.666-40.667-22.46 0-40.667 18.207-40.667 40.667s18.207 40.666 40.667 40.666zM106.75 106.75L84.638 84.638"
+        stroke="#C7C7C7"
+        strokeWidth={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default SvgFeatherSearch;
