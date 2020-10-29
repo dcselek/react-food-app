29ec6323000f59949c9c63aa52e788c84f409023
Doğan Can Selek <57669626+cselekk97@users.noreply.github.com>
2020-10-29 19:38:45 +0300

    umutbasalt

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Head from 'next/head'
+import Link from 'next/link'
 
 function HomePage() {
   return (
@@ -11,6 +12,38 @@ function HomePage() {
       </Head>
       <div>
         umutbasalt
+        <br></br>
+        <Link href="/login">
+          <a>Login Page</a>
+        </Link>
+        <br></br>
+        <Link href="/checkout-delivery">
+          <a>Delivery</a>
+        </Link>
+        <br></br>
+        <Link href="/checkout-payment">
+          <a>Payment</a>
+        </Link>
+        <br></br>
+        <Link href="/my-profile">
+          <a>My Profile</a>
+        </Link>
+        <br></br>
+        <Link href="/my-profile1">
+          <a>My Profile-1</a>
+        </Link>
+        <br></br>
+        <Link href="/history">
+          <a>History</a>
+        </Link>
+        <br></br>
+        <Link href="/orders">
+          <a>Orders</a>
+        </Link>
+        <br></br>
+        <Link href="/no-internet">
+          <a>No Internet</a>
+        </Link>
       </div>
     </div>
   );
