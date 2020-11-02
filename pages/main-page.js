2d80e7ef98110da26d32ac256a8e7487015aca11
Doğan Can Selek <57669626+cselekk97@users.noreply.github.com>
2020-11-03 00:44:35 +0300

    edit main-page

--- pages/main-page.js
+++ pages/main-page.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Head from 'next/head'
 import Layout from '../components/layout'
 import Navbar from '../components/menu/navbar'
+import TitleText from '../components/typography/title-text'
+import ButtonSearch from '../components/buttons/search-button'
+import TextButton from '../components/buttons/text-button';
+import Order from '../components/foods/order'
+
+import Kebab from '../public/kebab.jpg'
+import Doner from '../public/doner.jpg'
+import BottomMenu from '../components/menu/bottom-menu';
 function MainPage(props) {
     return (
         <div>
@@ -12,6 +20,22 @@ function MainPage(props) {
             <Layout>
                 <div>
                     <Navbar></Navbar>
+                    <TitleText>Delicious <br />
+                    food for you</TitleText>
+                    <ButtonSearch></ButtonSearch>
+                    <nav className="main-page-nav">
+                        <TextButton selected>Foods</TextButton>
+                        <TextButton >Drinks</TextButton>
+                        <TextButton >Snacks</TextButton>
+                        <TextButton >Sauces</TextButton>
+                    </nav>
+                    <div className="main-page-foods">
+                        <a>see more</a>
+                        <Order name="Kebab" price="17$" src={Kebab}></Order>
+                        <Order name="Doner" price="15$" src={Doner}></Order>
+                    </div>
+
+                    <BottomMenu></BottomMenu>
                 </div>
             </Layout>
         </div>
